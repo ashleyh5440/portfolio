d461853cc065b803968936567ea9338843c7c35a
Ashley <ashleyh5440@gmail.com>
2024-02-18 16:26:33 -0500

    added home and about pages

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+
+function About () {
+    return (
+        <section id="about-me">
+            <h1>About Me</h1>
+            <p>Marketing and copywriting professional by day and intrepid developer/code ninja by night.</p>
+            <h2>Contact Me</h2>
+            <p>Email: <a href="mailto:ashleyh5440@gmail.com">ashleyh5440@gmail.com</a></p>
+            <p>GitHub: <a href="https://github.com/ashleyh5440">ashleyh5440</a></p>
+        </section>
+    );
+}
+
+export default About;
